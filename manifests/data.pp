61c60af90d793e1b0d71f37e54079d1a8aa7e7c9
Adrien Thebo <adrien@puppetlabs.com>
2012-04-13 14:32:21 -0700

    Correct global variable names in data

--- manifests/data.pp
+++ manifests/data.pp
@@ -67,13 +67,13 @@ class pe_upgrade::data {
   if $::pe_upgrade_version { $version = $::pe_upgrade_version }
   else { $version = file('/opt/puppet/pe_version') }
 
-  if $::pe_upgrade_download_dir { $download_dir = $::pe_download_dir }
+  if $::pe_upgrade_download_dir { $download_dir = $::pe_upgrade_download_dir }
   else { $download_dir = "https://pm.puppetlabs.com/puppet-enterprise/${version}" }
 
   if $::pe_upgrade_checksum { $checksum = $::pe_upgrade_checksum }
   else { $checksum = '51555e4827effc5d180a53a9fb2ee8c9' }
 
-  if $::pe_upgrade_answersfile { $answersfile = $pe_answersfile }
+  if $::pe_upgrade_answersfile { $answersfile = $::pe_upgrade_answersfile }
   else { $answersfile = "pe_upgrade/answers/default-agent.txt.erb" }
 
   if $::pe_upgrade_timeout { $timeout = $::pe_upgrade_timeout }
