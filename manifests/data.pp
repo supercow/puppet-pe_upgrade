6a0224a5ffda902aab113db5e714abe8c4290674
Adrien Thebo <adrien@puppetlabs.com>
2012-04-13 14:24:06 -0700

    Default version to pe_version on master

--- manifests/data.pp
+++ manifests/data.pp
@@ -58,10 +58,10 @@
 #
 class pe_upgrade::data {
   if $::pe_upgrade_version { $version = $::pe_upgrade_version }
-  else { $version = '2.5.0' }
+  else { $version = file('/opt/puppet/pe_version') }
 
   if $::pe_upgrade_download_dir { $download_dir = $::pe_download_dir }
-  else { $download_dir = 'https://pm.puppetlabs.com/puppet-enterprise/2.5.0/' }
+  else { $download_dir = "https://pm.puppetlabs.com/puppet-enterprise/${version}" }
 
   if $::pe_upgrade_checksum { $checksum = $::pe_upgrade_checksum }
   else { $checksum = '51555e4827effc5d180a53a9fb2ee8c9' }
