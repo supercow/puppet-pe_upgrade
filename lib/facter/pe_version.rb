155bace029ea1a435a602f567505f58ccf574b85
Adrien Thebo <adrien@puppetlabs.com>
2012-11-01 16:47:30 -0700

    Determine PE version using fewer regexes

--- lib/facter/pe_version.rb
+++ lib/facter/pe_version.rb
@@ -4,18 +4,19 @@
 #
 # Parses the puppetversion fact and extracts the relevant substrings
 #
-{
-  :pe_version       => /\(Puppet Enterprise\s+([\d.]+)\)/,
-  :pe_major_version => /\(Puppet Enterprise\s+(\d+)\.\d+\.\d+\)/,
-  :pe_minor_version => /\(Puppet Enterprise\s+\d+\.(\d+)\.\d+\)/,
-  :pe_patch_version => /\(Puppet Enterprise\s+\d+\.\d+\.(\d+)\)/,
-}.each_pair do |fact_name, fact_regex|
 
+version_facts = {
+  :pe_version       => lambda { |ver| ver },
+  :pe_major_version => lambda { |ver| ver.split('.')[0] },
+  :pe_minor_version => lambda { |ver| ver.split('.')[1] },
+  :pe_patch_version => lambda { |ver| ver.split('.')[2] },
+}
+
+version_facts.each_pair do |fact_name, fact_resolution|
   Facter.add(fact_name) do
     setcode do
-      if Facter.value(:puppetversion).match(fact_regex)
-        $1
-      end
+      ver_string = Facter.value(:puppetversion).scan(/\d+\.\d+\.\d+/).last
+      fact_resolution.call(ver_string)
     end
   end
 end
