036581c39e2f6c9d35f262541ba35bbe1d45c5ae
Adrien Thebo <git@somethingsinistral.net>
2013-06-01 16:55:47 -0700

    Test pe_upgrade::staging on multiple platforms

--- spec/classes/staging_spec.rb
+++ spec/classes/staging_spec.rb
@@ -1,33 +1,38 @@
 require 'spec_helper'
 
 describe 'pe_upgrade::staging', :type => :class do
-  let(:facts) {{
-    'operatingsystem'      => 'CentOS',
-    'architecture'         => 'i386',
-    'pe_upgrade_extension' => 'tar.gz',
-  }}
 
-  let(:installer) { 'puppet-enterprise-2.5.3-el-6-i386' }
+  shared_examples_for 'staging the Puppet Enterprise installer package' do |context|
+    include_context context
 
-  let(:params) {{
-    'installer'    => installer,
-    'version'      => '2.5.3',
-    'download_dir' => 'https://download.dir',
-    'timeout'      => '300',
-    'staging_root' => '/opt/staging/pe_upgrade',
-  }}
+    let(:installer) { facts['pe_upgrade_installer'] }
+
+    let(:params) do
+      {
+        'installer'    => installer,
+        'version'      => '2.5.3',
+        'download_dir' => 'https://download.dir',
+        'timeout'      => '300',
+        'staging_root' => '/opt/staging/pe_upgrade',
+      }
+    end
 
 
-  it do
-    should contain_staging__file("#{installer}.tar.gz").with({
-      'source'  => "https://download.dir/2.5.3/#{installer}.tar.gz",
-      'timeout' => '300',
-    })
+    it do
+      should contain_staging__file("#{installer}.tar.gz").with({
+        'source'  => "https://download.dir/2.5.3/#{installer}.tar.gz",
+        'timeout' => '300',
+      })
+    end
+
+    it do
+      should contain_staging__extract("#{installer}.tar.gz").with({
+        'target' => '/opt/staging/pe_upgrade',
+      })
+    end
   end
 
-  it do
-    should contain_staging__extract("#{installer}.tar.gz").with({
-      'target' => '/opt/staging/pe_upgrade',
-    })
+  on_all_platforms do |platform|
+    it_behaves_like 'staging the Puppet Enterprise installer package', platform
   end
 end
