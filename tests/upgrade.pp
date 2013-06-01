518d7bc33c002b36557d020ee15cfb317f26cfa5
Adrien Thebo <git@somethingsinistral.net>
2013-06-01 16:04:20 -0700

    Remove old smoke test file

--- tests/upgrade.pp
+++
@@ -1,7 +0,0 @@
-class { 'pe_upgrade':
-  version      => '2.0.3',
-  answersfile  => "pe/answers/agent.txt.erb",
-  download_dir => 'https://pm.puppetlabs.com/puppet-enterprise/2.0.3',
-  timeout      => '3600',
-}
-
