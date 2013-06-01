3353e1deadce85552e23bfe95901b19c23084d4a
Adrien Thebo <git@somethingsinistral.net>
2013-06-01 16:21:37 -0700

    Remove shared contexts

--- spec/shared_contexts/centos-5-i386.rb
+++
@@ -1,9 +0,0 @@
-shared_context 'CentOS 5 i386' do
-  let(:facts) do
-    {
-      'operatingsystem'      => 'CentOS',
-      'hardwaremodel'        => 'i386',
-      'pe_upgrade_extension' => 'tar.gz',
-    }
-  end
-end
