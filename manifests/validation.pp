f4bda98aae8aee6d7b266edff89c22b67ff764c5
Adrien Thebo <git@somethingsinistral.net>
2013-06-01 21:54:37 -0700

    Use versioncmp in validation class

--- manifests/validation.pp
+++ manifests/validation.pp
@@ -5,8 +5,6 @@ class pe_upgrade::validation(
   $upgrade_master,
 ) {
 
-  include pe_upgrade
-
   if $::osfamily == 'Windows' {
     fail("osfamily 'Windows' is not currently supported")
   }
@@ -15,34 +13,14 @@ class pe_upgrade::validation(
     fail("Refusing to upgrade Puppet master ${::clientcert} without 'upgrade_master' set")
   }
 
-  # Perform version validation
-  $ver_split = split($version, '\.')
-  $desired_major_version = $ver_split[0]
-  $desired_minor_version = $ver_split[1]
-  $desired_patch_version = $ver_split[2]
-
   # If someone really really wants to force a downgrade, I respect their
   # decisions and trust they understand the implication of their actions.
   if $pe_version and ! $allow_downgrade {
     $errmsg = "Refusing to downgrade from ${::pe_version} to ${version} without 'force_upgrade' set"
-    if $desired_major_version < $pe_major_version {
-      # If requested major version is less than the current major version,
-      # fail
+    $compare = versioncmp($pe_version, $version)
+
+    if $compare == '1' {
       fail($errmsg)
     }
-    elsif $desired_major_version == $::pe_major_version {
-      # Check minor version to see if it's a potential downgrade.
-      if $desired_minor_version < $::pe_minor_version {
-        fail($errmsg)
-      }
-      elsif $desired_minor_version == $::pe_minor_version {
-        # Major and minor versions match, finally check patch version to check
-        # for downgrades.
-        if $desired_patch_version < $::pe_patch_version {
-          fail($errmsg)
-        }
-      }
-    }
   }
-  # Behold, staircase code!
 }
