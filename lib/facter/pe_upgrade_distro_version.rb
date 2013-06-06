4ea2cc58ca0afd1db90be1838679b461ae7a8cbc
Adrien Thebo <git@somethingsinistral.net>
2013-06-06 16:34:32 -0700

    Tighten regex for pe_upgrade_distro_version

--- lib/facter/pe_upgrade_distro_version.rb
+++ lib/facter/pe_upgrade_distro_version.rb
@@ -1,11 +1,11 @@
 Facter.add(:pe_upgrade_distro_version) do
   confine :osfamily => 'RedHat'
-  setcode { Facter.value(:operatingsystemrelease).sub(/(\d).*/, '\1') }
+  setcode { Facter.value(:operatingsystemrelease).sub(/^(\d+)\..*/, '\1') }
 end
 
 Facter.add(:pe_upgrade_distro_version) do
   confine :operatingsystem => 'Debian'
-  setcode { Facter.value(:operatingsystemrelease).sub(/(\d).*/, '\1') }
+  setcode { Facter.value(:operatingsystemrelease).sub(/^(\d+)\..*/, '\1') }
 end
 
 Facter.add(:pe_upgrade_distro_version) do
