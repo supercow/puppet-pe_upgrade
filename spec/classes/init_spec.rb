db9a1f4475d53544497a778fb5f14dcf307caa3c
Adrien Thebo <git@somethingsinistral.net>
2013-06-06 16:05:43 -0700

    Add more complete spec coverage for init.pp

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,7 +5,7 @@ describe 'pe_upgrade', :type => :class do
     {
       'servername'           => 'a server and such',
       'clientcert'           => 'wat',
-      'pe_upgrade_installer' => 'puppet-enterprise-2.3.1',
+      'pe_upgrade_installer' => 'puppet-enterprise-:version',
       'pe_upgrade_extension' => 'tar.gz',
       'pe_upgrade_version'   => '2.6.1',
     }
@@ -34,14 +34,37 @@ describe 'pe_upgrade', :type => :class do
     end
 
     describe 'when an upgrade is required' do
-      describe 'with default params' do
-        it do
-          should contain_class('pe_upgrade::staging')
-        end
+      before { facts['pe_version'] = '2.0.3' }
 
-        it do
-          should contain_class('pe_upgrade::execution')
-        end
+      let(:params) do
+        {
+          'version' => '2.8.1',
+        }
+      end
+
+      it do
+        should contain_class('pe_upgrade::staging').with({
+          'version'      => '2.8.1',
+          'installer'    => 'puppet-enterprise-:version',
+          'download_dir' => /pm\.puppetlabs\.com/,
+          'staging_root' => %r[/opt/staging],
+          'timeout'      => '3600',
+        })
+      end
+
+      it do
+        should contain_class('pe_upgrade::execution').with({
+          'version'       => '2.8.1',
+          'mode'          => 'upgrade',
+          'migrate_certs' => 'false',
+          'staging_root'  => %r[/opt/staging],
+          'installer'     => 'puppet-enterprise-:version',
+          'timeout'       => '3600',
+          'logfile'       => 'false',
+          'certname'      => 'wat',
+          'server'        => 'a server and such',
+          'answersfile'   => %r[pe_upgrade/answers],
+        })
       end
     end
   end
